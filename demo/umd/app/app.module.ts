9a05fc0f11408d10017ed90365d77fe07b831d3d
Eric Layne <eric_layne@outlook.com>
2018-03-23 14:05:05 -0400

    Switch to SSL

--- demo/umd/app/app.module.ts
+++ demo/umd/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-import { AlertmeModule } from '';
+import { AlertmeModule } from 'alertme';
 
 @NgModule({
   imports: [ BrowserModule, AlertmeModule ],
